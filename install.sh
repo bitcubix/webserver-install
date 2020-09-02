e4841d501369f13f4a4e80dfc84486d57226ee30
gabrielix29 <gabrielix29@protonmail.ch>
2020-09-02 11:44:26 +0200

    add DB config

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 # requirements
 apt update
 apt upgrade -y
-apt install ca-certificates apt-transport-https lsb-release curl nano unzip software-properties-common -y
+apt install ca-certificates apt-transport-https lsb-release curl nano unzip software-properties-common pwgen -y
 
 #apache2
 apt install apache2 -y
@@ -23,13 +23,35 @@ apt install php7.4 php7.4-cli php7.4-curl php7.4-gd php7.4-intl php7.4-json php7
 
 # mariaDB
 apt install mariadb-server -y
-mysql_secure_installation
+rootPasswordDB=$(pwgen -s 32 1)
+adminPasswordDB=$(pwgen -s 32 1)
+(echo ""; echo "y"; echo rootPasswordDB; echo "y"; echo "y"; echo "y"; echo "y") | mysql_secure_installation
 
 #phpmyadmin
-wget
 cd /usr/share
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
 unzip phpmyadmin.zip
 rm phpmyadmin.zip
 mv phpMyAdmin-*-all-languages phpmyadmin
+cd phpmyadmin
+cat > config.inc.php <<EOF
+\$cfg['blowfish_secret'] = '$(pwgen -s 32 1)';
+$cfg['Servers'][\$i]['controluser'] = 'pma';
+$cfg['Servers'][\$i]['controlpass'] = '$rootPasswordDB';
+
+$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';
+EOF
+mariadb < "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY 'password';"
+mariadb < "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' IDENTIFIED BY '$adminPasswordDB' WITH GRANT OPTION;";
+mariadb < sql/create_tables.sql
 chmod -R 0755 phpmyadmin
+mkdir /usr/share/phpmyadmin/tmp/
+wget https://raw.githubusercontent.com/gabrielix29/webserver-install/master/phpmyadmin.conf -P /etc/apache2/conf-available/
+cd /etc/apache2/conf-available/
+a2enconf phpmyadmin.conf
+systemctl restart apache2
+chown -R www-data:www-data /usr/share/phpmyadmin/
+
+echo -e "\033[42mInstall successfull\033[0m"
+
+exit
